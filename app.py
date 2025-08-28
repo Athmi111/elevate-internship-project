280877ab92c0d19199b2a78d85cd565e71fbe7ee
Athmi111 <160569213+Athmi111@users.noreply.github.com>
2025-08-28 19:12:27 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+import requests
+import pickle
+
+with open('movie_data.pkl', 'rb') as file:
+    movies, cosine_sim = pickle.load(file)
+    
+def get_recommendations(title, cosine_sim=cosine_sim):
+    idx = movies[movies['title'] == title].index[0]
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:11]  # Get top 10 similar movies
+    movie_indices = [i[0] for i in sim_scores]
+    return movies[['title', 'movie_id']].iloc[movie_indices]
+
+def fetch_poster(movie_id):
+    api_key = '7b995d3c6fd91a2284b4ad8cb390c7b8'  # Replace with your TMDB API key
+    url = f'https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}'
+    response = requests.get(url)
+    data = response.json()
+    poster_path = data['poster_path']
+    full_path = f"https://image.tmdb.org/t/p/w500{poster_path}"
+    return full_path
+
